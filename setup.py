f4ddc516f9cc38398e5376c514a17150d9874ebe
AxelGard <axel.gard@protonmail.com>
2020-08-09 22:25:21 +0200

    more setup stuff and config

--- setup.py
+++ setup.py
@@ -1,15 +1,20 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='cira',
     version='0.0.1',    
     description='A simpler library for the alapaca trade api',
     url='https://github.com/AxelGard/cira',
-    author='Axel Gar',
+    author='Axel Gard',
     author_email='axel.gard@tutanota.com',
     license='MIT',
     packages=['cira'],
     install_requires=['alpaca-trade-api'],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
 
     classifiers=[
         'Development Status :: 1 - Plannig',
@@ -17,5 +22,6 @@ setup(
         'License :: OSI Approved :: MIT',  
         'Operating System :: POSIX :: Linux',
         'Programming Language :: Python :: 3.5',
+
     ],
 )
