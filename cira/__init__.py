dd93a31dbdde25904c4028c6046567fe6c16e59d
AxelGard <axel.gard@protonmail.com>
2021-10-31 17:12:51 +0100

    updated -V

--- cira/__init__.py
+++ cira/__init__.py
@@ -14,6 +14,6 @@ from .exchange import Exchange
 from .stock import Stock
 from .portfolio import Portfolio
 
-__version__ = "2.1.1"
+__version__ = "2.1.2"
 __author__ = "Axel Gard"
 __credits__ = "alpaca.markets"
