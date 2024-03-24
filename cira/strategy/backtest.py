b2bbea190611e4c871432154d079245c19f677fc
Axel Gard <axel.gard@protonmail.com>
2024-03-24 17:31:19 +0100

    fixed more with imports

--- cira/strategy/backtest.py
+++ cira/strategy/backtest.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 from typing import List
-from . import Strategy, ByAndHold
+from .strategy import Strategy, ByAndHold
 
 
 FEE_RATE = 0.004 # this is what alpaca takes 
